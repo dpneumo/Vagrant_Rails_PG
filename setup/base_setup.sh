8e65549bf087737d7674d8e39eceb137d809927b
Mitchell C Kuppinger <dpneumo@gmail.com>
2015-03-02 00:11:28 -0600

    Run scripts from /vagrant/setup (reverted from commit 88453c53c90ef1e07f5c2e4a068760c3cd0de19b)

--- setup/base_setup.sh
+++ setup/base_setup.sh
@@ -11,20 +11,10 @@ echo =   base setup for vm
 echo =
 echo ===================================
 
-dir="/vagrant/setup"
-
-# As user: root
-mkdir -p /etc/provisions
-
+dir="$PWD"
 . "$dir/environmental_variables.sh"
 . "$dir/common_build_dependencies.sh"
 . "$dir/encodings.sh"
-
-. "$dir/app_user_as_linux_user.sh"
-
-. "$dir/nodejs.sh"
-
-. "$dir/postgres_bootstrap.sh"
 
 echo ---
 echo base setup for vm is complete!
